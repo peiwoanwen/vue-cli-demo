f6e23d88daaec3ef1fe58813619e6d53068a7a8d
peiwoanwen <C_aiyan@163.com>
2018-10-25 09:29:12 +0800

    vue-cli

---
+++ firstVue/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <el-button>提交</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
